a3f524ced33a3a01556a88fd34edbfaa8a5fefe2
NNGOCTOAN <njack8151@gmail.com>
2025-10-18 04:37:46 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GameCaro
+{
+    public partial class Form1 : Form
+    {
+        #region Properties
+        ChessBoardManager chessBoard;
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+            chessBoard = new ChessBoardManager(pnlchessboard,txbPlayerName,pctbMark);
+            chessBoard.EndedGame += ChessBoard_EndedGame;
+            chessBoard.PlayerMarked += ChessBoard_PlayerMarked;
+
+            prcbCoolDown.Step = Cons.COOL_DOWN_STEP;
+            prcbCoolDown.Maximum = Cons.COOL_DOWN_TIME;
+            prcbCoolDown.Value = 0;
+            tmCoolDown.Interval = Cons.COOl_DOWN_INTERVAL;
+
+            NewGame();
+        }
+
+        void EndGame()
+        {
+            tmCoolDown.Stop();
+            pnlchessboard.Enabled = false;
+            undoToolStripMenuItem.Enabled = false;
+            MessageBox.Show("Kết thúc");
+        }  
+
+        void NewGame()
+        {
+            prcbCoolDown.Value = 0;
+            tmCoolDown.Stop();
+            undoToolStripMenuItem.Enabled = true;
+            chessBoard.DrawChessBoard();
+        }
+       
+        void Quit()
+        {
+            Application.Exit();
+        }
+
+        void Undo()
+        {
+            chessBoard.Undo();
+        }
+
+        private void ChessBoard_PlayerMarked(object sender, EventArgs e)
+        {
+            tmCoolDown.Start();
+            prcbCoolDown.Value = 0;
+            
+        }
+
+        private void ChessBoard_EndedGame(object sender, EventArgs e)
+        {
+            EndGame();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pctbavatar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void prcbCoolDown_Click(object sender, EventArgs e)
+        {
+ 
+        }
+
+        private void tmCoolDown_Tick(object sender, EventArgs e)
+        {
+            prcbCoolDown.PerformStep();
+            if (prcbCoolDown.Value >= prcbCoolDown.Maximum)
+            {
+                EndGame();
+            }
+        }
+        
+
+        private void qQuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Quit();
+        }
+
+       
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có chắc chắn muốn thoát ", "Thông báo", MessageBoxButtons.OKCancel) != System.Windows.Forms.DialogResult.OK)
+            e.Cancel = true;
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NewGame();
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Undo();
+        }
+    }
+}
+
