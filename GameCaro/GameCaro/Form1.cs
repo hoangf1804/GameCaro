ee660d524281d56daddd45dec85a54d6b3c64571
VODUYTUAN2802 <voduytuan2802@gmail.com>
2025-11-13 15:21:35 +0700

    UPdatesocket manager,form1cs,socketdata,chessboard

---
+++ GameCaro/GameCaro/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Drawing;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace GameCaro
+{
+    public partial class Form1 : Form
+    {
+        #region Properties
+        ChessBoardManager chessBoard;
+        SocketManager socket;
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+            socket = new SocketManager();
+
+            chessBoard = new ChessBoardManager(pnlchessboard, txbPlayerName, pctbMark);
+            chessBoard.EndedGame += ChessBoard_EndedGame;
+            chessBoard.PlayerMarked += ChessBoard_PlayerMarked;
+
+            prcbCoolDown.Step = Cons.COOL_DOWN_STEP;
+            prcbCoolDown.Maximum = Cons.COOL_DOWN_TIME;
+            prcbCoolDown.Value = 0;
+            tmCoolDown.Interval = Cons.COOl_DOWN_INTERVAL;
+
+
+
+            NewGame();
+        }
+
+        void EndGame()
+        {
+            tmCoolDown.Stop();
+            pnlchessboard.Enabled = false;
+            undoToolStripMenuItem.Enabled = false;
+            MessageBox.Show("Kết thúc");
+        }
+
+        void NewGame()
+        {
+            prcbCoolDown.Value = 0;
+            tmCoolDown.Stop();
+            undoToolStripMenuItem.Enabled = true;
+            chessBoard.DrawChessBoard();
+        }
+
+        void Quit()
+        {
+            Application.Exit();
+        }
+
+        void Undo()
+        {
+            chessBoard.Undo();
+        }
+
+        private void ChessBoard_PlayerMarked(object sender, EventArgs e)
+        {
+            tmCoolDown.Start();
+            prcbCoolDown.Value = 0;
+
+        }
+
+        private void ChessBoard_EndedGame(object sender, EventArgs e)
+        {
+            EndGame();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pctbavatar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void prcbCoolDown_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tmCoolDown_Tick(object sender, EventArgs e)
+        {
+            prcbCoolDown.PerformStep();
+            if (prcbCoolDown.Value >= prcbCoolDown.Maximum)
+            {
+                EndGame();
+            }
+        }
+
+
+        private void qQuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Quit();
+        }
+
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có chắc chắn muốn thoát ", "Thông báo", MessageBoxButtons.OKCancel) != System.Windows.Forms.DialogResult.OK)
+                e.Cancel = true;
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NewGame();
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Undo();
+        }
+        private void btnLAN_Click(object sender, EventArgs e)
+        {
+            socket.IP = txbIP.Text;
+
+            if (!socket.ConnectServer())
+            {
+                socket.CreateServer();
+
+                Thread listenThread = new Thread(() =>
+                {
+                    while (true)
+                    {
+                        Thread.Sleep(500);
+                        try
+                        {
+                            Listen();
+                            break;
+                        }
+                        catch
+                        {
+
+                        }
+                    }
+                });
+                listenThread.IsBackground = true;
+                listenThread.Start();
+            }
+            else
+            {
+                Thread listenThread = new Thread(() =>
+                {
+                    Listen();
+                });
+                listenThread.IsBackground = true;
+                listenThread.Start();
+
+                socket.Send("Thông tin từ Client");
+            }
+
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            txbIP.Text = socket.GetLocalIPv4(NetworkInterfaceType.Wireless80211);
+
+            if (string.IsNullOrEmpty(txbIP.Text))
+            {
+                txbIP.Text = socket.GetLocalIPv4(NetworkInterfaceType.Ethernet);
+            }
+        }
+
+        void Listen()
+        {
+            string data = (string)socket.Receive();
+
+            DialogResult dialogResult = MessageBox.Show(data);
+        }
+
+
+    }
+}
+
